7447328425737efa8abf4646b1dafdcdbead01dd
Jackson L. Bryant <jackbryant5589@gmail.com>
2025-06-17 13:54:20 -0400

    Update pocketbase.ts

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,6 @@
 import PocketBase from "pocketbase";
 
-const baseUrl = import.meta.env.VITE_POCKETBASE_URL;
+const baseUrl = import.meta.env.PB_URL;
 const pb = new PocketBase(baseUrl);
 
 export { pb, baseUrl };
